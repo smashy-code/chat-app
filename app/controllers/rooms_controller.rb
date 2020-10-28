543667b49424ca0eb38e90b2037767422037b7bf
smashy-code <73206032+smashy-code@users.noreply.github.com>
2020-10-28 16:39:46 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,17 @@ class RoomsController < ApplicationController
  def new
   @room = Room.new
  end
+
+ def create
+  if @room.save
+    redirect_to root_path
+  else
+    render :new
+  end
+
+    private
+
+    def room_params
+      params.require(:room).permit(:name, user_ids: [])
+ end
 end
