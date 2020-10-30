efe5e4fbeb8833594e5811e305166c55bf46e0e8
smashy-code <73206032+smashy-code@users.noreply.github.com>
2020-10-30 12:05:45 +0900

    結合テストの実装　４〜

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ I18n.locale = "en"
 
 RSpec.configure do |config|
   config.include SignInSupport
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
